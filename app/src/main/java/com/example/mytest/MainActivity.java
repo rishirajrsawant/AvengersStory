ad48438def267c981ea7ddf74235fb03e9117733
rishirajrsawant <rishiraj.sawant97@gmail.com>
2019-10-06 21:08:03 +0100

    my second commit

--- app/src/main/java/com/example/mytest/MainActivity.java
+++ app/src/main/java/com/example/mytest/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.mytest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button goButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goButton = findViewById(R.id.button);
+
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, GoActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
